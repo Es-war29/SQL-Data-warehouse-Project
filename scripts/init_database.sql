2a367e1b64d0bcb83107117fba49b377f92e7b8e
Es-war29 <eswar99balajee99@gmail.com>
2025-09-30 17:19:41 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,12 @@
+/*
+Create a database with the name datawarehouse
+Create 3 schemas with names bronze, silver, gold
+*/
+-- creating database
+CREATE DATABASE DW_House;
+USE DATABASE DW_House;
+
+-- creating schema
+create schema bronze;
+create schema silver;
+create schema gold;
